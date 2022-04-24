ce212353491079045320b5f837f4005bb3d9e47c
Dmitry <dserdtseva@mail.ru>
2022-04-24 18:05:41 +0700

    Final Version 1.

--- app/src/main/java/com/example/reavixmobileapp/MainPageApp2.java
+++ app/src/main/java/com/example/reavixmobileapp/MainPageApp2.java
@@ -6,9 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import java.util.ArrayList;
 
@@ -20,7 +28,6 @@ public class MainPageApp2 extends AppCompatActivity {
     MyAdapter adapter;
 
     TextView txt_id;
-
 
 
 
@@ -36,6 +43,43 @@ public class MainPageApp2 extends AppCompatActivity {
         personal_data = new ArrayList<>();
         xaracteristika = new ArrayList<>();
 
+        RadarChart radarChart = findViewById(R.id.radarchart);
+
+        ArrayList<RadarEntry> visitorsFirstWebSite = new ArrayList<>();
+        visitorsFirstWebSite.add(new RadarEntry(70));
+        visitorsFirstWebSite.add(new RadarEntry(70));
+        visitorsFirstWebSite.add(new RadarEntry(80));
+        visitorsFirstWebSite.add(new RadarEntry(90));
+        visitorsFirstWebSite.add(new RadarEntry(100));
+        visitorsFirstWebSite.add(new RadarEntry(65));
+        visitorsFirstWebSite.add(new RadarEntry(82));
+        visitorsFirstWebSite.add(new RadarEntry(93));
+        visitorsFirstWebSite.add(new RadarEntry(100));
+        visitorsFirstWebSite.add(new RadarEntry(0));
+
+        RadarDataSet radarDataSet = new RadarDataSet(visitorsFirstWebSite,"Общий процент качества знаний");
+        radarDataSet.setColor(Color.RED);
+        radarDataSet.setLineWidth(2f);
+        radarDataSet.setValueTextColor(Color.BLACK);
+        radarDataSet.setValueTextSize(14f);
+
+
+
+        RadarData radarDataSet1 = new RadarData();
+        radarDataSet1.addDataSet(radarDataSet);
+
+
+        String[] levels = {"Январь","Февраль","Март","Апрель","Май","Июнь","Сентябрь","Октябрь","Ноябрь","Декабрь"};
+        XAxis xAxis = radarChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(levels));
+
+        radarChart.getDescription().setText("");
+        radarChart.setData(radarDataSet1);
+
+
+
+
+
         adapter = new MyAdapter( this ,name_grupp,biografia,personal_data,xaracteristika);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -50,8 +94,6 @@ public class MainPageApp2 extends AppCompatActivity {
                 txt_id.setText(cursor.getString(0));
             }
         }
-
-
     }
 
     @SuppressLint("Range")
